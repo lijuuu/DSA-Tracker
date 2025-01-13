bcd1cf1f806e8fb2e62e592a94efdb868c56ecd6
liju-github <lijuthomasliju03@gmail.com>
2025-01-13 20:00:04 +0530

    added vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import { createRoot } from 'react-dom/client'
 import DSAbasics from './DSAbasics.jsx'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Blind75List from './Blind75List.jsx';
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Analytics></Analytics>
     <Router>
       <Routes>
         <Route path='/' element={<DSAbasics />} />
